01677f0a112ac8c643f4ad1abf093310a380d318
I.M. Adil <73747557+Knoxo@users.noreply.github.com>
2021-06-04 12:51:50 +0600

    seo

---
+++ src/components/SEO.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import Head from "next/head"
+
+function SEO() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome</title>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <link rel="canonical" href="https://knoxo.github.io" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik&display=swap"
+          rel="stylesheet"
+        />
+        <meta property="og:url" content="https://knoxo.github.io" />
+        <meta property="og:title" content="Knoxo's Website" />
+        <meta property="og:description" content="Knoxo, A Developer Boi" />
+        <meta property="og:image" content="https://i.imgur.com/C56x0zh.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:alt" content="Knoxo" />
+        <meta property="og:image:width" content="420" />
+        <meta property="og:image:height" content="420" />
+        {/* Twitter Card Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Knoxo_dev" />
+      </Head>
+    </div>
+  )
+}
+
+export default SEO
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import React from "react"
 import Head from "next/head"
+import SEO from "@components/SEO"
 import Header from "@components/Header"
 import Footer from "@components/Footer"
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Welcome</title>
-        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Rubik&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <SEO />
       <main>
         <Header />
         <div className="mt-36 flex justify-center items-center">
