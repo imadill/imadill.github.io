8289a2691d731db5ae467a060af8e6f804543c51
I.M. Adil <73747557+Knoxo@users.noreply.github.com>
2021-06-28 16:34:02 +0600

    feat: gif to video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,15 +11,10 @@ export default function Home() {
       <main>
         <Header />
         <div className="mt-36 flex justify-center items-center">
-          <Image
-            src="/Rick.gif"
-            blurDataURL="data:image/gif;base64,LXHC1ko#.AM{-?bIM|ogWXj]V@of"
-            alt="Rick Astley"
-            placeholder="blur"
-            width={400}
-            height={300}
-          />
-          {/* eslint-enable */}
+          <video autoPlay loop muted playsInline width={400} height={300}>
+            <source src="/Rick.webm" type="video/webm" />
+            <source src="/Rick.mp4" type="video/mp4" />
+          </video>
         </div>
         <div className="mt-4 flex justify-center items-center">
           <h1 className="font-main sm:text-4xl text-3xl">
