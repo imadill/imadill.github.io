0c72448e1ffdf3edf1192a75d1069a024500d071
Knoxo <73747557+Knoxo@users.noreply.github.com>
2021-07-06 10:39:10 +0000

    Deploying to gh-pages from @ Knoxo/knoxo.github.io@a477e363cdf9db38e303826dcdab0e25f33f539f 🚀

---
+++ _next/static/CKtgSH63gf4Sy4dY-46d7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0cab6d836446f630baa8.js"],"/404":["static/chunks/pages/404-afa8f10bd25cccd1a128.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
