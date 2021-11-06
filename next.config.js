cca3b9bbbcdf56991f82a4b9ed0cc4e873d7a22e
I.M. Adil <73747557+Knoxo@users.noreply.github.com>
2021-11-06 12:44:27 +0600

    feat: added commitlint
    also removed nextjs static generation timeout

--- next.config.js
+++ next.config.js
@@ -2,4 +2,3 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 module.exports = withBundleAnalyzer({})
-module.exports = { experimental: { staticPageGenerationTimeout: 300 } }
